10ce77e1cc8e95d8a258b2b18ea31e9563871927
tatuana.bykava <tvbykovaa@gmail.com>
2023-01-04 12:47:01 +0300

    04.01 'parallax'

--- js/parallax.js
+++ js/parallax.js
@@ -5,7 +5,7 @@ let fog2 = document.querySelector('.mouse-parallax-fog-2');
 window.addEventListener('mousemove', function(e) {
 	let x = e.clientX / window.innerWidth;
 	let y = e.clientY / window.innerHeight;
-	bg.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
-	fog1.style.transform = 'translate(+' + x * 50 + 'px, -' + y * 50 + 'px)';
-	fog2.style.transform = 'translate(-' + x * 20 + 'px, -' + y * 20 + 'px)';
+	bg.style.transform = 'translate(-' + x * 30 + 'px, -' + y * 30 + 'px)';
+	fog1.style.transform = 'translate(+' + x * 10 + 'px, -' + y * 10 + 'px)';
+	fog2.style.transform = 'translate(-' + x * 10 + 'px, -' + y * 10 + 'px)';
 });
